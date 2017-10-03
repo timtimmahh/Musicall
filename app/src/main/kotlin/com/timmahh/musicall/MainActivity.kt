3722601a6f340c9ce86f025422a7d02744090d44
Tim <timtimmahh@gmail.com>
2017-10-02 20:49:53 -0400

    Moved service modules to app module. Updated gradle dependencies.

--- app/src/main/kotlin/com/timmahh/musicall/MainActivity.kt
+++ app/src/main/kotlin/com/timmahh/musicall/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.timmahh.musicall
 
-import android.arch.lifecycle.LifecycleRegistry
-import android.arch.lifecycle.LifecycleRegistryOwner
-import android.arch.lifecycle.Observer
+import android.app.Application
+import android.arch.lifecycle.*
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.view.View
+import com.timmahh.spotifyweb.getSimpleName
+import com.timmahh.spotifyweb.log
 import com.timmahh.spotifyweb.presenters.SpotifyDrawerLayout
 import com.timmahh.spotifyweb.toString
 import com.timmahh.spotifyweb.viewmodels.SpotifyUserViewModel
@@ -22,13 +26,26 @@ fun loadImage(view: ImageView, imageUrls: List<Image>) {
 
 @GlideModule
 class MainGlideModule : AppGlideModule()*/
+class MainViewModel
+@Inject
+constructor(application: Application) : AndroidViewModel(application) {
 
-class MainActivity : AppCompatActivity(), LifecycleRegistryOwner, AnkoLogger {
+    val navListeners: MediatorLiveData<MutableList<NavigationView.OnNavigationItemSelectedListener>> = MediatorLiveData()
+
+    init {
+        navListeners.value = ArrayList<NavigationView.OnNavigationItemSelectedListener>()
+    }
+
+}
+
+class MainActivity : AppCompatActivity(), LifecycleRegistryOwner, AnkoLogger, NavigationView.OnNavigationItemSelectedListener {
 
     val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
     lateinit var spotifyContainer: SpotifyDrawerLayout
     @Inject
     lateinit var spotifyUserViewModel: SpotifyUserViewModel
+    @Inject
+    lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +53,7 @@ class MainActivity : AppCompatActivity(), LifecycleRegistryOwner, AnkoLogger {
         (application as App).serviceComponent.inject(this)
         supportFragmentManager.beginTransaction().add(R.id.mainActivity, { spotifyContainer = SpotifyDrawerLayout(); spotifyContainer }.invoke(),
                 "SpotifyDrawerLayout").disallowAddToBackStack().commitNow()
+        mainViewModel.navListeners.value!!.add(spotifyContainer)
         (application as App).serviceComponent.inject(spotifyContainer)
         spotifyUserViewModel.spotifyUser.observe(this, Observer {
             debug("observing spotify user data")
@@ -57,9 +75,21 @@ class MainActivity : AppCompatActivity(), LifecycleRegistryOwner, AnkoLogger {
                     })
     }
 
+    fun serviceDropDown(v: View) {
+        log("Service Drop Down", v.getSimpleName())
+    }
+
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         debug("onNewIntent:" + intent?.extras)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        for (listener in mainViewModel.navListeners.value!!) {
+            listener.onNavigationItemSelected(item)
+        }
+        return true
     }
 
     override fun getLifecycle(): LifecycleRegistry = lifecycleRegistry
